c94b44b751c874c12038cf7b2d9072b470d9441e
yusei53 <yusei530922@gmail.com>
2023-12-25 22:12:29 +0900

    route.rbにルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
+  Rails.application.routes.draw do
+  root "static_pages#home"
+  get  "static_pages/home"
+  get  "static_pages/help"
   get  "static_pages/about"
-  root "application#hello"
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
